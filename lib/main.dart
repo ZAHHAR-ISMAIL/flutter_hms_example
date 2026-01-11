69f5e3ff2d8953090274ff65788b2f8b66d2336d
ZAHHAR-ISMAIL <zahhar.ismail@gmail.com>
2026-01-11 14:45:47 +0300

    map ok

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:huawei_push/huawei_push.dart';
 import 'package:flutter_hms_example/ScanPage.dart';
-import 'package:permission_handler/permission_handler.dart';
+// import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter_notification_channel/flutter_notification_channel.dart';
 import 'package:flutter_notification_channel/notification_importance.dart';
 
@@ -167,21 +167,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    void requestPerms(type) async {
-      Map<Permission, PermissionStatus> statuses = await [
-        Permission.locationWhenInUse,
-        Permission.locationAlways,
-      ].request();
+    // void requestPerms(type) async {
+    //   Map<Permission, PermissionStatus> statuses = await [
+    //     Permission.locationWhenInUse,
+    //     Permission.locationAlways,
+    //   ].request();
 
-      if (await Permission.location.status.isGranted) {
-        if (type == "Map")
-          Navigator.pushNamed(context, '/MapPage');
-        else
-          Navigator.pushNamed(context, '/LocationPage');
-      } else {
-        openAppSettings();
-      }
-    }
+    //   if (await Permission.location.status.isGranted) {
+    //     if (type == "Map")
+    //       Navigator.pushNamed(context, '/MapPage');
+    //     else
+    //       Navigator.pushNamed(context, '/LocationPage');
+    //   } else {
+    //     openAppSettings();
+    //   }
+    // }
 
     return Scaffold(
         appBar: AppBar(
@@ -194,13 +194,14 @@ class _MyHomePageState extends State<MyHomePage> {
               ElevatedButton(
                 child: const Text('Open Map Page'),
                 onPressed: () {
-                  requestPerms("Map");
+                  Navigator.pushNamed(context, '/MapPage');
+                  // requestPerms("Map");
                 },
               ),
               ElevatedButton(
                 child: const Text('Open Location Page'),
                 onPressed: () {
-                  requestPerms("Location");
+                  // requestPerms("Location");
                 },
               ),
               ElevatedButton(
